ee5360ac7f9b123f90cbb47ebcd8c9873db5678b
Sri <rajamails19@gmail.com>
2023-10-13 12:32:23 -0700

    Create Frequency.java
    Frequency of a specific number

---
+++ Frequency.java
@@ -0,0 +1,36 @@
+package javaBasicPrograms;
+
+import java.util.Arrays;
+
+class Frequency
+{
+    public static void main (String[] args) {
+        int[] arr = {5, 8, 5, 7, 8, 10};
+        int size = arr.length;
+        countFrequency(arr, size);
+    }
+
+    static void countFrequency(int[] arr, int n)
+    {
+
+        Arrays.sort(arr);
+
+        // Traverse the sorted array
+        for (int i = 0; i < n; i++)
+        {
+            int count = 1;
+
+            // Move the index ahead whenever
+            // you encounter duplicates
+            while (i < n - 1 && arr[i] == arr[i + 1]) {
+                i++;
+                count++;
+            }
+
+            System.out.println(arr[i] + ": " + count);
+
+
+            count++;
+        }
+    }
+}
